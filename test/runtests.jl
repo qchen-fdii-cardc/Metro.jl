0d7f05000467acc4f9cf5a53ec32372339f716c3
withstand <qchen2015@hotmail.com>
2026-06-10 20:31:13 +0800

    TDD start

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,8 @@ using Test
 
 @testset "Metro.jl" begin
     # Write your tests here.
+
+    @test true  # Placeholder test, replace with actual tests.
+
+    @test 0.0 == 0.0  # Example test, replace with actual tests.tes
 end
